38373d992e1d008b01294fd8268c934bf074abc4
Riphah-Dev <dev@riphah.edu.pk>
2026-03-09 22:55:00 +0500

    chore: add files (4 files)

---
+++ faculty-clearance-system/frontend/src/components/Departments/Phase1/Lab/LabClearance.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import LabClearanceEnhanced from './LabClearanceEnhanced';
+
+export default function LabClearance({ onComplete }) {
+  return <LabClearanceEnhanced onComplete={onComplete} />;
+}
