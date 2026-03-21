dbffb14e413a01bc7f060dd45d0d4e576d90a340
Riphah-Dev <dev@riphah.edu.pk>
2026-03-21 05:11:00 +0500

    chore: add files (4 files)

---
+++ faculty-clearance-system/frontend/src/components/Departments/Phase3/ORIC/ORICClearance.js
@@ -0,0 +1,7 @@
+import ORICClearanceEnhanced from './ORICClearanceEnhanced';
+
+export default function ORICClearance({ onComplete }) {
+  return (
+    <ORICClearanceEnhanced onComplete={onComplete} />
+  );
+}
